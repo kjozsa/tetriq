2cb0dd6487f4c55c9e136b3de39e48880ed0a052
Kristof Jozsa <kristof.jozsa@gmail.com>
2012-09-03 07:56:16 +0200

    ++

--- src/main/scala/org/fsdev/tetriq/Main.scala
+++ src/main/scala/org/fsdev/tetriq/Main.scala
@@ -23,10 +23,10 @@ case object Z extends Tile((0, 0), (1, 0), (1, 1), (2, 1))
 case class Score()
 
 // the moving block
-class Block(val tileForm: Tile, start: Position) {
-  var positions: Seq[Position] = tileForm.position map (_ + start)
+class Piece(val form: Tile, start: Position) {
+  var positions: Seq[Position] = form.position map (_ + start)
 
-  override def toString = tileForm.getClass().getSimpleName() + " at " + positions
+  override def toString = form.getClass().getSimpleName() + " at " + positions
 
   def tick { positions = positions map (_ + (0, 1)) }
   def moveLeft { positions = positions map (_ + (-1, 0)) }
@@ -34,37 +34,42 @@ class Block(val tileForm: Tile, start: Position) {
 }
 
 // occupied positions + active block
-class GameBoard(val blocks: Seq[Position], val current: Block) {
+class GameBoard(val blocks: Seq[Position], val current: Piece) {
   val (sizeX, sizeY) = (10, 8)
-  override def toString() = (
+  override def toString() = "\n" + (
     for {
       y <- 0 to sizeY;
       x <- 0 to sizeX
       pos = Position(x, y)
-    } yield if (view.contains(pos)) "X" else "." + (if (x == sizeX) "\n" else "")).mkString
+    } yield if (view contains pos) "X" else "." + (if (x == sizeX) "\n" else "")).mkString
 
   def view = blocks ++ current.positions
 
-  def add(newBlock: Block): Either[Score, GameBoard] = {
-    if (view intersect newBlock.positions isEmpty) {
-      Right(new GameBoard(view, newBlock))
+  def add(newPiece: Piece): Either[Score, GameBoard] = {
+    if (view intersect newPiece.positions isEmpty) {
+      Right(new GameBoard(view ++ current.positions, newPiece))
     } else Left(new Score)
+  }
+
+  def tick: Either[Score, GameBoard] = {
+    val newpos = current.positions map (_ + (0, 1))
+    if (blocks intersect newpos isEmpty) {
+      current.tick
+      Right(this)
+    } else {
+      add(new Piece(O, (1, 0))) // TODO random
+    }
   }
 }
 
 object Main extends App {
-  val board = new GameBoard(Seq(), new Block(O, (5, 0)))
+  var board = new GameBoard(Seq((2, 3), (3, 3), (4, 3), (5, 3), (6, 3)), new Piece(T, (5, 0)))
   println(board)
 
-  board.current.tick
-  println(board)
-
-  board.current.moveLeft
-  println(board)
-
-  board.current.tick
-  println(board)
-
-  board.current.moveLeft
-  println(board)
+  var state = board.tick
+  
+//  for {
+//	  i <- 1 to 5 
+//    state <- state.right
+//  } yield println(state.tick)
 }
